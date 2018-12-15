15ad8ce6869821dbe54f1c54dd57a2a235b8faeb
chengbeyond <chengbeyond@hotmail.com>
2018-12-15 21:08:27 +0800

    huawei

--- huawei/spiders/run.py
+++ huawei/spiders/run.py
@@ -3,5 +3,5 @@
 from scrapy.cmdline import execute
 
 execute(('scrapy crawl hw').split())
-# execute(('scrapy view http://www.xicidaili.com/nn/').split())
-# execute(('scrapy shell https://www.dytt8.net/html/gndy/dyzz/20181205/57895.html').split())
+# execute(('scrapy view https://www.vmall.com').split())
+# execute(('scrapy shell https://www.vmall.com').split())
